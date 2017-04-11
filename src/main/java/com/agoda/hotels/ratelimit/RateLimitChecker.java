e172177a0b7275fa44298d00e968814254c04131
amanmanocha <amanmanocha@yahoo.com>
2017-04-12 01:43:06 +0530

    Tests for rate limits

--- src/main/java/com/agoda/hotels/ratelimit/RateLimitChecker.java
+++ src/main/java/com/agoda/hotels/ratelimit/RateLimitChecker.java
@@ -1,26 +1,26 @@
 package com.agoda.hotels.ratelimit;
 class RateLimitChecker {
-  private final int maxLimit;
+  private final int limit;
   private final TimeProvider timeProvider;
   
-  private long currentSecond = 0;
-  private int countForCurrentSecond = 0;
+  private long lastCheckedSeconds = 0;
+  private int hitsForLastCheckedSeconds = 0;
 
   public RateLimitChecker(TimeProvider timeProvider, int maxLimit) {
     this.timeProvider = timeProvider;
     if(maxLimit == 0) 
       maxLimit = Integer.MAX_VALUE;
-    this.maxLimit = maxLimit;
+    this.limit = maxLimit;
   }
 
   public synchronized boolean checkWithinRateLimits() {
-    final long now = timeProvider.currentSeconds();
-    if (currentSecond == now) {
-      countForCurrentSecond++;
+    final long currentSeconds = timeProvider.currentSeconds();
+    if (lastCheckedSeconds == currentSeconds) {
+      hitsForLastCheckedSeconds++;
     } else {
-      currentSecond = now;
-      countForCurrentSecond = 1;
+      lastCheckedSeconds = currentSeconds;
+      hitsForLastCheckedSeconds = 1;
     }
-    return countForCurrentSecond <= maxLimit;
+    return hitsForLastCheckedSeconds <= limit;
   }
 }
