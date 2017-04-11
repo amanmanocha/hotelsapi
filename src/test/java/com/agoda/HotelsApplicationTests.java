fc7b2bb9cc78799d4325cf6d696606cbef601d5b
amanmanocha <amanmanocha@yahoo.com>
2017-04-11 11:57:49 +0530

    Test setup is created

--- src/test/java/com/agoda/HotelsApplicationTests.java
+++ src/test/java/com/agoda/HotelsApplicationTests.java
@@ -1,16 +1,53 @@
 package com.agoda;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.nio.charset.Charset;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+import com.agoda.hotels.HotelsApplication;
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = HotelsApplication.class)
+@WebAppConfiguration
+@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
 public class HotelsApplicationTests {
+  private MediaType contentType = new MediaType("application", "hal+json", Charset.forName("UTF-8"));
 
-	@Test
-	public void contextLoads() {
-	}
+  @Autowired
+  private WebApplicationContext webApplicationContext;
 
+  private MockMvc mockMvc;
+
+  @Before
+  public void setup() {
+      mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+              .build();
+  }
+
+  @Test
+  public void validate_get_address() throws Exception {
+
+      mockMvc.perform(get("/hotels").header("x-api-key", "1"))
+              .andExpect(status().isOk())
+              .andExpect(
+                      content().contentType(contentType))
+              .andExpect(jsonPath("$.street").value("12345 Horton Ave"));
+
+  }
 }
---
+++ src/test/java/com/agoda/TestAppConfiguration.java
@@ -0,0 +1,9 @@
+package com.agoda;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@TestConfiguration
+@ComponentScan("com.agoda.hotels")
+public class TestAppConfiguration {
+}
